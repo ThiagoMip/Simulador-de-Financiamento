e8898a3496cbcb268bb255fc50dc1f12218f6149
Thiago Prieto <thiagomirp@gmail.com>
2022-07-17 00:39:04 -0300

    Simulador Financiamento part1

---
+++ index.js
@@ -0,0 +1,11 @@
+const carencia = document.querySelector('#comCarencia');
+const listaSuspensa = document.querySelector('#listaSuspensa');
+
+comCarencia.addEventListener('change', function(){
+    if(this.checked){
+        listaSuspensa.removeAttribute('hidden');
+    }
+    else{
+        listaSuspensa.setAttribute('hidden', 'hidden');
+    }
+});
